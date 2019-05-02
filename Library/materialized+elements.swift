0609b77b52cea7a6e1cb11aa4cfae575a1fa1288
yimajo <y.imajo@gmail.com>
2019-05-02 11:36:39 +0900

    elements,errorsをRxSwift5のconpactMapに変更

--- Library/materialized+elements.swift
+++ Library/materialized+elements.swift
@@ -9,15 +9,13 @@
 import Foundation
 import RxSwift
 
-extension RxSwift.ObservableType where E: RxSwift.EventConvertible {
+extension RxSwift.ObservableType where Element: RxSwift.EventConvertible {
 
-    public func elements() -> RxSwift.Observable<E.ElementType> {
-        return filter { $0.event.element != nil }
-            .map { $0.event.element! }
+    public func elements() -> RxSwift.Observable<Element.Element> {
+        return compactMap { $0.event.element }
     }
 
     public func errors() -> RxSwift.Observable<Swift.Error> {
-        return filter { $0.event.error != nil }
-            .map { $0.event.error! }
+        return compactMap { $0.event.error }
     }
 }
